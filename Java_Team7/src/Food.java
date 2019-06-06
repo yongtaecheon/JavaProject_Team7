7b204c8e0f1f4c36811b2633896a2e1af24bf920
jinju <wjbbdq1223@naver.com>
2019-06-06 18:23:09 +0900

    Food

--- Java_Team7/src/Food.java
+++ Java_Team7/src/Food.java
@@ -1,4 +1,13 @@
 
 public class Food {
-
+		String name;
+		int kcal;
+		double ch; //Åº
+		double protein;//´Ü
+		double fat;//Áö
+		double sugal;//´ç·ù
+		double na;//³ªÆ®·ý
+		Food(){
+		Food ary[] = new Food[500];
+		}
 }
