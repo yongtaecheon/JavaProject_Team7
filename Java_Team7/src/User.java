157006442e1e43b488b27c42720b91a01c8769d3
user <user@LAPTOP-0B6VMU37>
2019-06-06 18:03:08 +0900

    second

--- Java_Team7/src/User.java
+++ Java_Team7/src/User.java
@@ -1,4 +1,21 @@
 
 public class User {
-
+	private double height;
+	private double weight;
+	private char gender;//m = male(남자), f = female(여자)
+	
+	public User() {
+		height = 0;
+		weight = 0;
+		gender = 'm';
+	}
+	public User(double height, double weight, char gender) {
+		this.height = height;
+		this.weight = weight;
+		this.gender = gender;
+	}
+	public void getHeight() {
+		
+	}
+	
 }
