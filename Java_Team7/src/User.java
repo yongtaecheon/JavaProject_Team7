ebe059158117e458e7c548442f28af68dd415ee3
yongtaecheon <ide2143@naver.com>
2019-06-18 20:23:17 +0900

    committ

--- Java_Team7/src/User.java
+++ Java_Team7/src/User.java
@@ -1,65 +1,121 @@
 
 public class User {
-	private double height;
-	private double weight;
-	private char gender;//m = male(남자), f = female(여자)
-	private double BMI;
-	public User() {
-		height = 0;
-		weight = 0;
-		gender = 'm';
-		BMI=0;
-	}
-	public User(double height, double weight, char gender) {
-		this.height = height;
-		this.weight = weight;
-		this.gender = gender;
-	}
-	public double setHeight(double height) {
-	
-		return height;
-	}
-	public void getHeight()
-	{
-		this.height=height;
-	}
-	public double setWeight(double weight)
-	{
-		return weight;
-	}
-	public void getWeight()
-	{
-		this.weight=weight;
-	}
-	public double setBMI(double height,double weight)
-	{
-		return BMI=weight/(height*height);
-	}
-	public void getBMI()
-	{
-		this.BMI=BMI;
-	}
-	public String Bmi()
-	{
-		if(20<=BMI && BMI<=25 )
-		{
-			return ("정상");
-		}
-		else if(25<=BMI && BMI<=30)
-		{
-			return ("과체중");
-		}
-		else if(30<=BMI && BMI<=40)
-		{
-			return ("비만");
-		}
-		else if( BMI> 40)
-		{
-			return ("고도비만");
-		}
-		else
-		{
-			return ("저체중");
-		}
-	}
+   private double height;
+   private double weight;
+   private String gender;//m = male(남자), f = female(여자)
+   private double BMI;
+   private double calory;
+   private String[] food;
+   private int foodsize;//food배열의 크기
+   public User() {
+      height = 0;
+      weight = 0;
+      gender = "m";
+      BMI=0;
+      calory=0;
+      food = new String[50];
+      foodsize = 0;
+   }
+   public User(double height, double weight, String gender) {
+      this.height = height;
+      this.weight = weight;
+      this.gender = gender;
+      food = new String[50];
+      foodsize = 0;
+   }
+   public String getGender(){
+      return gender;
+   }
+   public void setGender(String gender) {
+      this.gender=gender;
+   }
+   
+   public double getHeight() {
+      return height;
+   }
+   public void setHeight(double height) {
+      this.height=height;
+   }
+   public double getWeight() {
+      return weight;
+   }
+   public void setWeight(double weight){
+      this.weight=weight;
+   }
+   public double getCalory(){
+      return calory;
+   }
+   public void setCalory(double calory){
+      this.calory=calory;
+   }
+   public double getBMI(){
+      return BMI;
+   }
+   public void setBMI(double BMI) {
+	   this.BMI = BMI;
+   }
+   public void setBMI(double height,double weight){
+      this.BMI=weight/(height*height);
+   }
+   public void setFood(String foodName) {
+	   food[foodsize++]=foodName;
+   }
+   public String[] getFood() {
+	   return food;
+   }
+   public void calculateBMI()
+   {
+      if(20<=BMI && BMI<25 )
+      {
+         System.out.printf("BMI지수가 %.2f 이므로 정상입니다.", getBMI());
+      }
+      else if(25<=BMI && BMI<30)
+      {
+    	  System.out.printf("BMI지수가 %.2f 이므로 과체중입니다.", getBMI());
+      }
+      else if(30<=BMI && BMI<40)
+      {
+    	  System.out.printf("BMI지수가 %.2f 이므로 비만입니다.", getBMI());
+      }
+      else if( BMI> 40)
+      {
+    	  System.out.printf("BMI지수가 %.2f 이므로 고도비만입니다.", getBMI());
+      }
+      else
+      {
+    	  System.out.printf("BMI지수가 %.2f 이므로 저체중입니다.", getBMI());
+      }
+   }
+   public String calculateCalory(String gender)
+   {
+      if(gender.equals("m")) {//남자
+         if(calory > 2700)
+         {
+            return ("하루 권장 칼로리보다 많습니다");
+         }
+         else if(calory == 2700)
+         {
+            return ("하루 권장 칼로리입니다");
+         }
+         else 
+         {
+            return ("하루 권장 칼로리 보다 작습니다");
+         }
+      }
+      else {//여자
+         if(calory > 2000)
+         {
+            return ("하루 권장 칼로리보다 많습니다");
+         }
+         else if(calory == 2000)
+         {
+            return ("하루 권장 칼로리입니다");
+         }
+         else 
+         {
+            return ("하루 권장 칼로리 보다 작습니다");
+         }
+      }
+   }
+   
 }
