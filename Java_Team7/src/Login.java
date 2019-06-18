d5bbc92d5dc27c87e6da18bd5ec55be156916b86
yongtaecheon <ide2143@naver.com>
2019-06-19 00:25:36 +0900

    login updated

--- Java_Team7/src/Login.java
+++ Java_Team7/src/Login.java
@@ -2,21 +2,68 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Login{
+	static User u = new User();
+	static Scanner keyboard = new Scanner(System.in);
 	
 	public static void main(String args[]) {
-		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Enter Username: ");
 		String name = keyboard.next();
 		System.out.print("Enter Password: ");
 		String password = keyboard.next();
 		loadFile(name, password);
+		addFood(name);
+		System.out.println(getCal(name));
+	}
+	public static double getCal(String name) {//name.txt파일의 음식을 읽고 칼로리르 계산하여 출력
+		Food f = new Food();
+		f.readFoodFile();
+		Scanner inputStream = null;
+		try {
+			inputStream = new Scanner(new FileInputStream(name+".txt"));
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Error opening the File.");
+		}
+		while(inputStream.hasNextLine()) {
+			if(inputStream.next().equals("<음식>")){
+				break;
+			}
+		}
+		while(inputStream.hasNext()) {
+			u.setFood(inputStream.next());
+		}
+		//칼로리계산
+		for(int i = 0; i < u.foodsize; i++) {
+			for(int j = 0; j < 50; j++) {
+				if((u.food[i]).equals(f.food[j].getName())) {
+					u.calory+= f.food[j].getKcal();
+				}
+			}
+		}
+		inputStream.close();
+		return u.calory;
+	}
+	
+	public static void addFood(String name) {//append모드로 기존의 파일을 열어서 음식을 추가
+		PrintWriter outputStream = null;
+		try{
+			outputStream = new PrintWriter(new FileOutputStream(name+".txt", true));
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Error opening the File.");
+		}
+		System.out.println("음식 이름 입력:");
+		while(true) {
+			String input = keyboard.next();
+			if(input.equals("끝")) {
+				break;
+			}
+			outputStream.println(input);
+		}
+		outputStream.close();
 	}
 	
 	public static void createFile(String name, String password) {
-		Scanner keyboard = new Scanner(System.in);
-		User u = new User();
-		Food f = new Food();
-		f.readFoodFile();
 		PrintWriter outputStream  = null;
 		try {
 			outputStream = new PrintWriter(new FileOutputStream(name+".txt"));
@@ -30,57 +77,48 @@ public class Login{
 		u.setWeight(keyboard.nextDouble());
 		u.setGender(keyboard.next()); 
 		u.setBMI(u.getHeight(), u.getWeight());
-		outputStream.println(u.getHeight()); outputStream.println(u.getWeight()); outputStream.println(u.getGender()); outputStream.println(u.getBMI());
-		System.out.println("음식 이름 입력:");
-		while(true) {
-			String input = keyboard.next();
-			if(input.equals("끝")) {
-				break;
-			}
-			u.setFood(input);
-			outputStream.println(u.getFood());
-		}
-		//칼로리계산
-		for(int i = 0; i < u.foodsize; i++) {
-			for(int j = 0; j < 50; j++) {
-				if((u.food[i]).equals(f.food[j].getName())) {
-					u.calory+= f.food[j].getKcal();
-				}
-			}
-		}
-		outputStream.println(u.getCalory());
+		outputStream.println("<키>"); outputStream.println(u.getHeight());
+		outputStream.println("<몸무게>"); outputStream.println(u.getWeight());
+		outputStream.println("<성별>"); outputStream.println(u.getGender());
+		outputStream.println("<BMI지수>"); outputStream.println(u.getBMI());
+		outputStream.println("<음식>"); 
 		outputStream.close();
 	}
 	//이미 있으면 불러오고 없으면 새로운 텍스트 파일을 생성
-	public static void loadFile(String name, String password) {
-		Scanner keyboard = new Scanner(System.in);
-		User u = new User();
+	public static User loadFile(String name, String password) {
 		Scanner inputStream = null;
 		try {
 			inputStream = new Scanner(new FileInputStream(name+".txt"));
 			String scan_password = inputStream.next();
 				if(scan_password.equals(password)) {
-					u.setHeight(inputStream.nextDouble());
-					u.setWeight(inputStream.nextDouble());
-					u.setGender(inputStream.next());
-					u.setBMI(inputStream.nextDouble());
-					while(inputStream.hasNextLine()) {
-						if(inputStream.hasNextDouble()) {
-							u.setCalory(inputStream.nextDouble());
-							break;
+					if(inputStream.next().equals("<키>")) {
+						u.setHeight(inputStream.nextDouble());	
+					}
+					if(inputStream.next().equals("<몸무게>")) {
+						u.setWeight(inputStream.nextDouble());
+					}
+					if(inputStream.next().equals("<성별>")) {
+						u.setGender(inputStream.next());
+					}
+					if(inputStream.next().equals("<BMI>")) {
+						u.setBMI(inputStream.nextDouble());
+					}
+					if(inputStream.next().equals("<음식>")) {
+						while(inputStream.hasNextLine()) {
+							u.setFood(inputStream.next());
 						}
-						u.setFood(inputStream.next());
 					}
 				}
 				else {
 						System.out.println("잘못된 비밀번호 입니다.");
 				}
-			}
+			inputStream.close();
+		}
 		catch(FileNotFoundException e){
 			System.out.println("새로운 사용자 이시군요! 계정을 새로 생성하겠습니다.");
 			createFile(name, password);
-			loadFile(name,password);
 		}
+		return u;
 	}
 	
 }
